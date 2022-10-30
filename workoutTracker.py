5508fdf899beeb0e4771c0caaa38223fd735c615
codeRunner5k <andre.anglin8@gmail.com>
2022-10-30 11:37:25 -0400

    updated RunningData.csv with more entries

--- workoutTracker.py
+++ workoutTracker.py
@@ -6,6 +6,7 @@ Created on Tue Oct 25 13:44:48 2022
 """
 import pandas as pd
 import statistics
+from datetime import datetime, timedelta
 #createDataFrameCSV(...) is used to create a data frame from a given csv
 #If not given a data frame uses the runningData.csv file
 #returns a df off of runningData.csv
@@ -23,9 +24,27 @@ def createDFwithSpecficColumns(df,colWanted=["Distance_Miles","AVG_Pace","Date",
 def getDataFrameInfo(df):
     return df.info()
 
+
+#for col in ("Gun_time", "Net_time", "Pace"):
+#    df[col] = pd.to_timedelta(df[col])
+def convertTimeInfo(df):
+    convertTimeLst = []
+    for col in range(len(df)):
+        convertTimeLst.append(pd.to_timedelta(df[col]))
+    return convertTimeLst
+
+
 df = createDataFrameCSV()
 df2 = createDFwithSpecficColumns(df)
+dfDuration = df2["Duration"]
+print (df[8])
+
 print("Mean of Distance ran is " + str(statistics.mean(df2["Distance_Miles"])) + " miles")
 print("Mode of Distance ran is "+ str(statistics.mode(df2["Distance_Miles"])) + " miles")
 print("Median of Distance ran is " + str(statistics.median(df2["Distance_Miles"]))+ " miles")
+#dfDuration = convertTimeInfo(dfDuration)
+
+#print("Mean of Distance ran is " + str(dfDuration.mean()) + " miles")
+#print("Mode of Distance ran is "+ str(statistics.mode(dfDuration)) + " miles")
+#print("Median of Distance ran is " + str(statistics.median(dfDuration))+ " miles")
 #print (getDataFrameInfo(df2))
